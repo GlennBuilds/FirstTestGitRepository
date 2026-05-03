0bdbd95afe888757b281ef0d1959972fd4477a2d
GlennBuilds <p6h4f9t6kq@privaterelay.appleid.com>
2026-05-03 14:44:20 +0200

    Fix app icon cropping and route button layout

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'balkan2026-v11';
+const CACHE_NAME = 'balkan2026-v12';
 const MAP_CACHE_NAME = 'balkan2026-map-v1';
 const BASE_PATH = new URL('./', self.location.href).pathname;
 
